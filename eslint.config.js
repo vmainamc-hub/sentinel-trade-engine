7743b4719cfb5ac98148cd42a0b18b12d62f8d9d
vmainamc-hub <vmainamc@gmail.com>
2026-09-19 20:31:15 +0300

    chore: update eslint configuration and env variables
    Relax linting rules for development and add cron secret keys to environment configuration.

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 import js from "@eslint/js";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import eslintConfigPrettier from "eslint-config-prettier";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
@@ -34,7 +34,10 @@ export default tseslint.config(
       ],
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-empty": ["error", { allowEmptyCatch: true }],
+      "prefer-const": "off",
     },
   },
-  eslintPluginPrettier,
+  eslintConfigPrettier,
 );
